9239200397f3f8f75a2988c044e8c24e4dfbb45e
Rorisang Putu <rorisangputu@gmail.com>
2025-05-29 14:39:18 +0200

    feat: implement user login functionality with JWT token generation and cookie handling

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,30 @@
+import User from "../models/user.model.js";
+import { GenerateSignature, passwordCompare } from "../utility/userUtility.js";
 
-export const userLogin = (req, res) => {
+export const userLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
+        const user = await User.findOne({ email: email });
+
+        if (!user) return res.status(400).json({ message: "User not found" });
+
+        const isMatch = await passwordCompare(password, user.password);
+        if (!isMatch) return res.status(400).json({ message: "Username/Password dont match" })
+        
+        const token = await GenerateSignature(user._id.toString());
+
+        res.cookie("cira_user_auth_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+        });
+
+        return res.status(200).json({message: "User Logged In", user: user._id})
+  
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export const userLogout = (req, res) => {
