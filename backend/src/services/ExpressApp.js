dfad517169c28228a83fd7a73b5e80df20434445
FrankCodedIt <franklyu6@gmail.com>
2025-05-29 17:07:32 +0200

    refactor: standardize import statements and formatting in ExpressApp.js

--- backend/src/services/ExpressApp.js
+++ backend/src/services/ExpressApp.js
@@ -1,29 +1,27 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cookieParser from "cookie-parser";
 //import cors from 'cors'
 
-import userRoutes from '../routes/user.routes.js'
-import incidentRoutes from '../routes/incident.route.js'
+import userRoutes from "../routes/user.routes.js";
+import incidentRoutes from "../routes/incident.route.js";
+import userAuthRoute from "../routes/userAuth.route.js";
 export default async (app) => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
 
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(cookieParser())
+  // app.use(cors({
+  //     origin: process.env.FRONTEND_URL,
+  //     credentials: true,
+  // }));
 
-    // app.use(cors({
-    //     origin: process.env.FRONTEND_URL,
-    //     credentials: true,
-    // }));
+  app.use("/api/user", userRoutes);
+  app.use("/api/incident", incidentRoutes);
+  app.use("/api/auth/user", userAuthRoute);
 
-    
-    app.use('/api/user', userRoutes)
-    app.use('/api/incident', incidentRoutes)
-    
-    console.log("Helllllooo")
-    app.get('/', (req, res) => {
-        console.log('In slash route');
-        res.send({ message: "Hello from / route" });
-    });
-    
-    
-}
+  console.log("Helllllooo");
+  app.get("/", (req, res) => {
+    console.log("In slash route");
+    res.send({ message: "Hello from / route" });
+  });
+};
