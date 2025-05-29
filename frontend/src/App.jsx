b31debaf9104f96e163d43fc358d5d65dd51e276
BonganiChauke <bonganichauke11@outlook.com>
2025-05-29 18:09:06 +0200

    Import mdn bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import Layout from "./Layout/Layout"
 import Home from "./pages/Home"
+import SignIn from "./pages/user/SignIn"
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 
 function App() {
 
@@ -14,6 +16,22 @@ function App() {
           element={
             <Layout>
               <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Layout>
+              <SignIn />
             </Layout>
           }
         />
