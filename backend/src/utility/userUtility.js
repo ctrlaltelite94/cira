a057a5c858a5980c5207e720a74157c84c205453
Rorisang Putu <rorisangputu@gmail.com>
2025-05-29 14:39:13 +0200

    feat: add password comparison utility function

--- backend/src/utility/userUtility.js
+++ backend/src/utility/userUtility.js
@@ -1,4 +1,9 @@
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 export const GenerateSignature = async (adminId) => {
     return jwt.sign({ adminId }, process.env.JWT_SECRET_KEY, { expiresIn: "7d" });
+};
+
+export const passwordCompare = async (password, hashed) => {
+  return await bcrypt.compare(password, hashed);
 };
