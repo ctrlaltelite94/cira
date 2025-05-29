c6aa25c3c81f346f7e972fe514a1a8c18af115d5
Rorisang Putu <rorisangputu@gmail.com>
2025-05-29 13:55:38 +0200

    fix: remove unused getIncident route and clean up incident.route.js

--- backend/src/routes/incident.route.js
+++ backend/src/routes/incident.route.js
@@ -1,12 +1,8 @@
 import express from 'express'
-import { createIncident, getIncident } from '../controllers/incident.controller.js';
+import { createIncident  } from '../controllers/incident.controller.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json("Hellllooo")
-    //console.log({message: "Hello from inc route"})
-})
 router.post('/create', createIncident);
 
 
