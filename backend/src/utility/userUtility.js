a0fbf2a4cdbbc77f0e344073ca8d25d85870f88b
Rorisang Putu <rorisangputu@gmail.com>
2025-05-29 22:59:01 +0200

    feat: rename and enhance user signature generation functions for clarity

--- backend/src/utility/userUtility.js
+++ backend/src/utility/userUtility.js
@@ -1,7 +1,12 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
-export const GenerateSignature = async (userId) => {
+
+export const GenerateUserSignature = async (userId) => {
     return jwt.sign({ userId }, process.env.JWT_SECRET_KEY, { expiresIn: "7d" });
+};
+
+export const GenerateResSignature = async (responderId) => {
+    return jwt.sign({ responderId }, process.env.JWT_SECRET_KEY, { expiresIn: "7d" });
 };
 
 export const passwordCompare = async (password, hashed) => {
