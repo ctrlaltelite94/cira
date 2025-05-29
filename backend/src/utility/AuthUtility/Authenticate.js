45c9df8086444d69f303fc0271feebc2a0c224d9
Rorisang Putu <rorisangputu@gmail.com>
2025-05-29 15:07:11 +0200

    fix: add missing newline at the end of Authenticate.js file

--- backend/src/utility/AuthUtility/Authenticate.js
+++ backend/src/utility/AuthUtility/Authenticate.js
@@ -10,4 +10,17 @@ export const ValidateUserSignature = async (req) => {
     } catch {
       return null;
     }
-  };
+};
+
+export const ValidateResponderSignature = async (req) => {
+    const token = req.cookies["cira_user_auth_token"];
+    if (!token) return null;
+  
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+      req.admin = decoded;
+      return decoded;
+    } catch {
+      return null;
+    }
+};
