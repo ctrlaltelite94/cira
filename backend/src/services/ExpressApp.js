d4a2068394bed376b8850169b9ecfdaab2c0a3e5
Rorisang Putu <rorisangputu@gmail.com>
2025-05-29 11:29:32 +0200

    feat: set up database connection and refactor server initialization

--- backend/src/services/ExpressApp.js
+++ backend/src/services/ExpressApp.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
+import userRoutes from '../routes/user.routes.js'
 export default async (app) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser())
 
+    app.use(cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+    }));
+
+    app.use('/api/user', userRoutes)
     
 }
