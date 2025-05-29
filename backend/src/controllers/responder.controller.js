3348afc7f3e54eacfe8bcb4c0ed4110e5806310f
Rorisang Putu <rorisangputu@gmail.com>
2025-05-30 01:00:07 +0200

    feat: implement incident retrieval and update functionality for responders

--- backend/src/controllers/responder.controller.js
+++ backend/src/controllers/responder.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import Responder from "../models/responder.model.js";
 import { GenerateStationCode } from "../utility/responderUtility.js";
+import Incident from "../models/incident.model.js";
 
 export const responderRegister = async (req, res) => {
     try {
@@ -33,6 +34,74 @@ export const responderRegister = async (req, res) => {
 
 }
 
-export const getIncidents = async (req, res) => {
-    res.json('You made it to get responder incident')
+export const getAllIncidents = async (req, res) => {
+    try {
+        const id = req.responder.responderId;
+
+        const responder = await Responder.findById(id);
+        const type = responder.type
+
+        const incidents = await Incident.find({
+            $or: [
+              { requestedResponse: type },
+              { requestedResponse: "Both" }
+            ]
+          });
+          
+
+        if (incidents.length === 0) return res.status(400).json({ message: `There are no incidents for ${type}` })
+    
+        return res.status(200).json({ message: `Incidents for ${type}`, incidents });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getIncidentById = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const incident = await Incident.findById(id);
+
+        if(!incident) return res.status(400).json({message: "Incident not found"})
+
+        return res.status(200).json({message: "Incident found", incident})
+    } catch (error) {
+        console.log(error);
+    }
+    //const responderId = req.responder.responderId;
+}
+
+export const updateIncident = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const { status, etr } = req.body;
+        
+
+        const responderId = req.responder.responderId;
+        const validStatuses = ["Reported", "Responding", "Resolved", "Cancelled"];
+            
+        if (!validStatuses.includes(status)) {
+            return res.status(400).json({ message: "Invalid status" });
+        }
+
+
+        const updatedIncident = await Incident.findByIdAndUpdate(
+            id,
+            {
+                status: status,
+                etr: etr,
+                respondingService: responderId
+            }, { new: true }
+        );
+
+        if (!updatedIncident) {
+            return res.status(404).json({ message: "Incident not found" });
+        }
+        return res.status(200).json(updatedIncident);
+    } catch (error) {
+        console.log(error); 
+        res.status(500).json({ message: "Error updating status" });
+    }
+    
 }
