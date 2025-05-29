c468557bbec202ee9382f2b0b42ef2b32d8ddd99
Rorisang Putu <rorisangputu@gmail.com>
2025-05-29 14:39:23 +0200

    fix: correct import statement for auth.controller.js and ensure proper export of router

--- backend/src/routes/userAuth.route.js
+++ backend/src/routes/userAuth.route.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { userLogin, userLogout } from '../controllers/auth.controller';
+import { userLogin, userLogout } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/login', userLogin )
-router.post('/logout', userLogout )
+router.post('/logout', userLogout)
+
+export default router;
