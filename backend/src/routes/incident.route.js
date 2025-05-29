79b52e30d397fd2ade741f524405cac2a24fd07e
Rorisang Putu <rorisangputu@gmail.com>
2025-05-29 15:08:04 +0200

    feat: add getAllIncidents route to incident router

--- backend/src/routes/incident.route.js
+++ backend/src/routes/incident.route.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { createIncident  } from '../controllers/incident.controller.js';
+import { createIncident, getAllIncidents  } from '../controllers/incident.controller.js';
 import { Authenticate } from '../middleware/userAuth.js';
 
 const router = express.Router();
 
 router.post('/create', Authenticate, createIncident);
+
+router.get('/', getAllIncidents)
 
 
 
