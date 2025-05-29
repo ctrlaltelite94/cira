695f5ea530a4290cbdd2580bbadcda6e98acad21
Rorisang Putu <rorisangputu@gmail.com>
2025-05-29 22:58:15 +0200

    feat: implement responder login and logout functionality with token management

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
-import { GenerateSignature, passwordCompare } from "../utility/userUtility.js";
+import Responder from '../models/responder.model.js'
+import { GenerateUserSignature, GenerateResSignature, passwordCompare } from "../utility/userUtility.js";
 
 export const userLogin = async (req, res) => {
     try {
@@ -12,7 +13,7 @@ export const userLogin = async (req, res) => {
         const isMatch = await passwordCompare(password, user.password);
         if (!isMatch) return res.status(400).json({ message: "Username/Password dont match" })
         
-        const token = await GenerateSignature(user._id.toString());
+        const token = await GenerateUserSignature(user._id.toString());
 
         res.cookie("cira_user_auth_token", token, {
             httpOnly: true,
@@ -30,6 +31,44 @@ export const userLogin = async (req, res) => {
 export const userLogout = (req, res, next) => {
     try {
         res.cookie("cira_user_auth_token", "", {
+            expires: new Date(0),
+        })
+
+        return res.status(200).json({message: "User logged out"})
+    } catch {
+        next(error)
+    }
+}
+
+export const responderLogin = async (req, res) => {
+    try {
+        const { stationCode, password } = req.body;
+
+        const responder = await Responder.findOne({ stationCode: stationCode });
+
+        if (!responder) return res.status(400).json({ message: "Responder not found" });
+
+        const isMatch = await passwordCompare(password, responder.password);
+        if (!isMatch) return res.status(400).json({ message: "Username/Password dont match" })
+        
+        const token = await GenerateResSignature(responder._id.toString());
+
+        res.cookie("cira_responder_auth_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+        });
+
+        return res.status(200).json({message: "Responder Logged In", responder: responder._id})
+  
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const responderLogout = (req, res, next) => {
+    try {
+        res.cookie("cira_responder_auth_token", "", {
             expires: new Date(0),
         })
 
