b56fc8883f5a8da737ce2a502fb4b2cf89992b97
Rorisang Putu <rorisangputu@gmail.com>
2025-05-30 03:20:53 +0200

    feat: add responder authentication routes for login and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Layout from "./Layout/Layout"
 import Home from "./pages/Home"
 import SignIn from "./pages/user/SignIn"
 import SignUp from "./pages/user/SignUp";
+import ResponderSignIn from "./pages/responder/ResponderSignIn";
+import ResponderSignUp from "./pages/responder/ResponderSignUp";
 
 function App() {
 
@@ -32,6 +34,22 @@ function App() {
           element={
             <Layout>
               <SignIn />
+            </Layout>
+          }
+        />
+        <Route
+          path="/responder/login"
+          element={
+            <Layout>
+              <ResponderSignIn />
+            </Layout>
+          }
+        />
+        <Route
+          path="/responder/signup"
+          element={
+            <Layout>
+              <ResponderSignUp />
             </Layout>
           }
         />
