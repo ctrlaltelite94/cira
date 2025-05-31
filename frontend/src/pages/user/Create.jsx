629f51cd38af4d8c03644a0356d9cee5ca67847f
Rorisang Putu <rorisangputu@gmail.com>
2025-05-31 21:47:07 +0200

    fix: refactor Create component for improved layout and styling using Tailwind CSS

--- frontend/src/pages/user/Create.jsx
+++ frontend/src/pages/user/Create.jsx
@@ -106,9 +106,10 @@ const Create = () => {
 
 
     return (
-        <div className="mt-5">
-            {refNum ? (
-                <div className='py-5 h-screen'>
+        <div className="h-screen">
+            <div className='flex flex-col justify-center items-center py-10'>
+                {refNum ? (
+
                     <div className="alert alert-success text-center py-5">
                         <h4 className="mb-2 text-success">{message} successfully</h4>
 
@@ -131,136 +132,138 @@ const Create = () => {
                             </Link>
                         </div>
                     </div>
-                </div>
-            ) : (
-                <>
-                    <h2 className='pt-5'>Create New Incident</h2>
-                    <hr />
-                    <form onSubmit={onSubmit} className='py-4'>
 
-                        <div className="mb-3">
-                            <label className="form-label">
-                                Title
+                ) : (
+                    <>
+                        <h2 className='pt-5'>Create New Incident</h2>
+                        <hr />
+                        <form onSubmit={onSubmit} className='py-4'>
+
+                            <div className="mb-3">
+                                <label className="form-label">
+                                    Title
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        name="title"
+                                        {...register("title", { required: "This field is required" })}
+                                    />
+                                    {errors.title && (
+                                        <span className="text-red-700">{errors.title.message}</span>
+                                    )}
+                                </label>
+
+                            </div>
+
+                            <div className="mb-3">
+                                <label className="form-label">
+                                    Description
+                                    <textarea
+                                        type="text"
+                                        className="form-control"
+                                        name="description"
+                                        {...register("description", { required: "This field is required" })}
+                                    />
+                                    {errors.description && (
+                                        <span className="text-red-700">{errors.description.message}</span>
+                                    )}
+                                </label>
+
+                            </div>
+
+                            <div className="mb-3">
+                                <p className="form-label font-semibold mb-2">Incident Type</p>
+                                {IncidentTypes.map((type, index) => (
+                                    <label
+                                        key={type}
+                                        htmlFor={`incident-type-${index}`}
+                                        className="inline-flex items-center mr-4 cursor-pointer"
+                                    >
+                                        <input
+                                            id={`incident-type-${index}`}
+                                            type="radio"
+                                            value={type}
+                                            className="form-radio text-blue-600 mx-4"
+                                            {...register("incidentType", {
+                                                required: "This field is required",
+                                            })}
+                                        />
+                                        <span className="ml-2">{type}</span>
+                                        {errors.incidentType && (
+                                            <span className="text-red-700">{errors.incidentType.message}</span>
+                                        )}
+                                    </label>
+                                ))}
+                                {errors.incidentType && (
+                                    <span className="text-red-600 text-sm font-bold block mt-1">
+                                        {errors.incidentType.message}
+                                    </span>
+                                )}
+                            </div>
+
+
+                            <div className="mb-3">
+                                <label htmlFor="address" className="form-label">
+                                    Address <span className="text-muted">(Optional)</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    id="address"
+                                    className="form-control"
+                                    {...register("address")}
+                                />
+
+                            </div>
+
+
+                            {/* Location */}
+                            <div className="mb-3">
+                                <label className="form-label">Location</label>
                                 <input
                                     type="text"
                                     className="form-control"
-                                    name="title"
-                                    {...register("Title", { required: "This field is required" })}
+                                    id="location"
+                                    value={
+                                        formData.location.coordinates.length
+                                            ? `${formData.location.coordinates[1]}, ${formData.location.coordinates[0]}`
+                                            : ''
+                                    }
+                                    readOnly
                                 />
-                                {errors.title && (
-                                    <span className="text-red-700">{errors.title.message}</span>
-                                )}
-                            </label>
+                                <button type="button" onClick={getLocation} className="btn btn-secondary mt-2">
+                                    Get My Location
+                                </button>
+                                <div className="form-text text-muted">{locationStatus}</div>
+                            </div>
 
-                        </div>
 
-                        <div className="mb-3">
-                            <label className="form-label">
-                                Description
-                                <textarea
-                                    type="text"
-                                    className="form-control"
-                                    name="description"
-                                    {...register("description", { required: "This field is required" })}
-                                />
-                                {errors.description && (
-                                    <span className="text-red-700">{errors.description.message}</span>
-                                )}
-                            </label>
 
-                        </div>
-
-                        <div className="mb-3">
-                            <p className="form-label font-semibold mb-2">Incident Type</p>
-                            {IncidentTypes.map((type, index) => (
-                                <label
-                                    key={type}
-                                    htmlFor={`incident-type-${index}`}
-                                    className="inline-flex items-center mr-4 cursor-pointer"
-                                >
-                                    <input
-                                        id={`incident-type-${index}`}
-                                        type="radio"
-                                        value={type}
-                                        className="form-radio text-blue-600 mx-4"
-                                        {...register("incidentType", {
-                                            required: "This field is required",
-                                        })}
-                                    />
-                                    <span className="ml-2">{type}</span>
-                                    {errors.incidentType && (
-                                        <span className="text-red-700">{errors.incidentType.message}</span>
+                            <div className="mb-3">
+                                <label className="form-label">
+                                    Requested Response
+                                    <select
+                                        className="form-select"
+                                        {...register("requestedResponse", { required: "This field is required" })}
+                                    >
+                                        <option value="">Select</option>
+                                        <option value="Police">Police</option>
+                                        <option value="Ambulance">Ambulance</option>
+                                        <option value="Both">Both</option>
+                                    </select>
+                                    {errors.requestedResponse && (
+                                        <span className="text-red-700">{errors.requestedResponse.message}</span>
                                     )}
                                 </label>
-                            ))}
-                            {errors.incideType && (
-                                <span className="text-red-600 text-sm font-bold block mt-1">
-                                    {errors.incideType.message}
-                                </span>
-                            )}
-                        </div>
+                            </div>
 
 
-                        <div className="mb-3">
-                            <label htmlFor="address" className="form-label">
-                                Address <span className="text-muted">(Optional)</span>
-                            </label>
-                            <input
-                                type="text"
-                                id="address"
-                                className="form-control"
-                                {...register("address")}
-                            />
-
-                        </div>
-
-
-                        {/* Location */}
-                        <div className="mb-3">
-                            <label className="form-label">Location</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="location"
-                                value={
-                                    formData.location.coordinates.length
-                                        ? `${formData.location.coordinates[1]}, ${formData.location.coordinates[0]}`
-                                        : ''
-                                }
-                                readOnly
-                            />
-                            <button type="button" onClick={getLocation} className="btn btn-secondary mt-2">
-                                Get My Location
-                            </button>
-                            <div className="form-text text-muted">{locationStatus}</div>
-                        </div>
-
-
-
-                        <div className="mb-3">
-                            <label className="form-label">
-                                Requested Response
-                                <select
-                                    className="form-select"
-                                    {...register("requestedResponse", { required: "This field is required" })}
-                                >
-                                    <option value="">Select</option>
-                                    <option value="Police">Police</option>
-                                    <option value="Ambulance">Ambulance</option>
-                                    <option value="Both">Both</option>
-                                </select>
-                                {errors.requestedResponse && (
-                                    <span className="text-red-700">{errors.requestedResponse.message}</span>
-                                )}
-                            </label>
-                        </div>
-
-
-                        <button type="submit" className="btn btn-primary mb-5">Create Incident</button>
-                    </form>
-                </>
-            )}
-        </div>
+                            <button type="submit" className="btn btn-primary mb-5">Create Incident</button>
+                        </form>
+                    </>
+                )
+                }
+            </div>
+        </div >
     );
 };
 
